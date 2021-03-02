d3ffedb3f351213254d6546705c237f88896d026
Omar Alhalaika <omar.alhalaika@yahoo.com>
2021-03-02 16:41:07 +0200

    compliting the lab 02 and start with lab 03 requrments!

--- js/app.js
+++ js/app.js
@@ -6,77 +6,83 @@ let userName=prompt(`What can I call you dear?`);
 alert(`Welcome `+userName+`, it is my pleasure to know you!`);
 
 
-function background(background) {
+function background() {
     let myBackground=prompt(`Try to guess this! \nDo I have an IT background? \n (yes/no)?`);
     
-if (myBackground.toLowerCase ==`yes`||myBackground.toLowerCase ==`y`) {
+if (myBackground.toLowerCase() ==`yes`||myBackground.toLowerCase() ==`y`) {
     //console.log(`Great job `+userName)
     alert(`Great job `+userName)
-}else if (myBackground.toLowerCase ==`no`||myBackground.toLowerCase ==`n`) {
+}else if (myBackground.toLowerCase() ==`no`||myBackground.toLowerCase() ==`n`) {
     alert (`Actully I do have an IT background`);
     
-}else  myBackground=prompt(`Try to guess this! \nDo I have an IT background? \n (yes/no)?`);
-
+}else  {
+background();}
 }
+
 background();
 
-function experience(background) {
+function experience() {
     let myExperience=prompt(`Try to guess this too! \nDo I have more than 5 years of experience? \n (yes/no)?`);
     
-if (myExperience.toLowerCase ==`yes`||myExperience.toLowerCase ==`y`) {
+if (myExperience.toLowerCase() ==`yes`||myExperience.toLowerCase() ==`y`) {
     //console.log(`Great job `+userName)
     alert(`Great job `+userName)
-}else if (myExperience.toLowerCase ==`no`||myExperience.toLowerCase ==`n`) {
+}else if (myExperience.toLowerCase() ==`no`||myExperience.toLowerCase() ==`n`) {
     alert (`Actully I do have more than 5 years of experience`);
     
-}else  myExperience=prompt(`Try to guess this too! \nDo I have more than 5 years of experience? \n (yes/no)?`);
-
+}else  {
+experience();}
 }
 experience();
 
-function hobbies(background) {
+function hobbies() {
     let myHobbies=prompt(`Try to guess this! \nDo you think Art is one of my fav. hobbies? \n (yes/no)?`);
     
-if (myHobbies.toLowerCase ==`yes`||myHobbies.toLowerCase ==`y`) {
+if (myHobbies.toLowerCase() ==`yes`||myHobbies.toLowerCase() ==`y`) {
     //console.log(`Great job `+userName)
     alert(`Well done `+userName)
-}else if (myHobbies.toLowerCase ==`no`||myHobbies.toLowerCase ==`n`) {
+}else if (myHobbies.toLowerCase() ==`no`||myHobbies.toLowerCase() ==`n`) {
     alert (`In fact it is`);
     
-}else  myHobbies=prompt(`Try to guess this! \nDo you think Art is one of my fav. hobbies? \n (yes/no)?`);
-
+}else  {
+hobbies();}
 }
 hobbies();
 
-function spanish(background) {
+function spanish() {
     let mySpanish=prompt(`Try to guess this! \nDo you think I can speak Spanish? \n (yes/no)?`);
     
-if (mySpanish.toLowerCase ==`yes`||mySpanish.toLowerCase ==`y`) {
+if (mySpanish.toLowerCase() ==`yes`||mySpanish.toLowerCase() ==`y`) {
     //console.log(`Great job `+userName)
     alert(`Great job `+userName)
-}else if (mySpanish.toLowerCase ==`no`||mySpanish.toLowerCase ==`n`) {
+}else if (mySpanish.toLowerCase() ==`no`||mySpanish.toLowerCase() ==`n`) {
     alert (`I wish to! \nbut I can't`);
     
-}else  mySpanish=prompt(`Try to guess this! \nDo you think I can speak Spanish? \n (yes/no)?`);
-
+}else {
+spanish();}
 }
 spanish();
 
-function common(background) {
+function common() {
     let incommon=prompt(`Try to guess this! \nDo you think that we have anything in common? \n (yes/no)?`);
     
-if (incommon.toLowerCase ==`yes`||incommon.toLowerCase ==`y`) {
+if (incommon.toLowerCase() ==`yes`||incommon.toLowerCase() ==`y`) {
     //console.log(`Great job `+userName)
     alert(`Yes, I think we do `+userName)
-}else if (incommon.toLowerCase ==`no`||incommon.toLowerCase ==`n`) {
+}else if (incommon.toLowerCase() ==`no`||incommon.toLowerCase() ==`n`) {
     alert (`Maybe \nbut I think there is something in common`);
     
-}else  incommon=prompt(`Try to guess this! \nDo you think that we have anything in common? \n (yes/no)?`);
-
+}else {
+common();}
 }
-spanish();
+common();
 
-alert(`It has been my pleasure taking you in a tour about who I am; ` +userName)
+alert(`It has been my pleasure taking you in a tour about who I am; ` +userName);
+
+let counter=1;
+while (counter=4) {
+    
+}
     
     
 
